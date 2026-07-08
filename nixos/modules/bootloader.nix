c7e673241efdbb302ddcbc6c59e3d1b9c7655698
Ahmed <ssdcv101@gmail.com>
2026-07-08 22:36:13 +0300

    Feature(nixos): structure optimal systemd boot kernel runtime modules flags

---
+++ nixos/modules/bootloader.nix
@@ -0,0 +1,15 @@
+# Bootloader configuration (GRUB - no theme).
+{ pkgs, ... }: {
+  boot.kernelPackages = pkgs.linuxPackages_6_18;
+
+  boot.loader = {
+    systemd-boot.enable = false;
+    grub = {
+      enable = true;
+      device = "nodev";
+      efiSupport = true;
+      efiInstallAsRemovable = false;
+    };
+    efi.canTouchEfiVariables = true;
+  };
+}
