f21d860e24d8db2b082d16997524c9b93659e4b8
Ahmed <ssdcv101@gmail.com>
2026-07-08 22:36:09 +0300

    Feature(nixos): link native desktop communication channels for sandboxed programs

---
+++ nixos/modules/xdg-desktop-portal.nix
@@ -0,0 +1,49 @@
+# XDG Desktop Portal Configuration
+# This module provides proper configuration for xdg-desktop-portal services
+# optimized for niri Wayland compositor (uses GNOME portal for ScreenCast)
+{ pkgs, ... }: {
+  # Install xdg-desktop-portal and related packages
+  environment.systemPackages = with pkgs; [
+    xdg-desktop-portal
+    xdg-desktop-portal-gtk
+    xdg-desktop-portal-gnome # GNOME portal for ScreenCast/Screenshot (niri-compatible)
+    xdg-utils
+    thunar # Thunar file manager
+    tumbler # Thumbnail service for Thunar
+  ];
+
+  # Set default portal implementations for Wayland/niri
+  environment.sessionVariables = {
+    XDG_CURRENT_DESKTOP = "niri";
+    XDG_SESSION_DESKTOP = "niri";
+    XDG_SESSION_TYPE = "wayland";
+    GTK_USE_PORTAL = "1";
+    NO_AT_BRIDGE = "1";
+  };
+
+  # Configure portal implementations for niri
+  xdg.portal = {
+    enable = true;
+    extraPortals = with pkgs; [
+      xdg-desktop-portal-gtk
+      xdg-desktop-portal-gnome # niri implements GNOME ScreenCast portal protocol
+    ];
+    config = {
+      common = {
+        # Prefer GNOME portal as primary backend for niri Wayland sessions.
+        # Keep GTK only for FileChooser to avoid early-session GTK backend churn.
+        default = [ "gnome" ];
+        "org.freedesktop.impl.portal.FileChooser" = [ "gtk" ];
+        "org.freedesktop.impl.portal.Screenshot" = [ "gnome" ];
+        "org.freedesktop.impl.portal.ScreenCast" = [ "gnome" ];
+      };
+    };
+  };
+
+  systemd.user.services = {
+    xdg-desktop-portal-gtk.serviceConfig = {
+      Restart = "on-failure";
+      RestartSec = "1s";
+    };
+  };
+}
