e1dce3caa32eab71a195bdc9734b501fa68b8bb8
Ahmed <ssdcv101@gmail.com>
2026-07-08 22:36:08 +0300

    Feature(nixos): configure touch tracking gesture responses on input interfaces

---
+++ nixos/modules/libinput.nix
@@ -0,0 +1,4 @@
+# Libinput configuration.
+# This module enables libinput for advanced input device support,
+# providing better touchpad, mouse, and touch device handling.
+{ services.libinput.enable = true; }
