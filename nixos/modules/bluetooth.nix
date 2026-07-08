6a9b9f1bcd619772900cd9667016d32fc610039c
Ahmed <ssdcv101@gmail.com>
2026-07-08 22:36:14 +0300

    Feature(nixos): map hardware controller states to strict interface policies

---
+++ nixos/modules/bluetooth.nix
@@ -0,0 +1,33 @@
+# Bluetooth configuration module.
+{
+  config,
+  lib,
+  pkgsStable,
+  ...
+}:
+{
+  # Custom module options for Bluetooth configuration
+  options.mySystem.bluetooth = {
+    enable = lib.mkEnableOption "Bluetooth support with Blueman manager";
+
+    powerOnBoot = lib.mkOption {
+      type = lib.types.bool;
+      default = false;
+      description = "Power on Bluetooth adapter automatically at boot";
+    };
+  };
+
+  # Configuration applied when Bluetooth is enabled
+  config = lib.mkIf config.mySystem.bluetooth.enable {
+    hardware.bluetooth = {
+      enable = true;
+      inherit (config.mySystem.bluetooth) powerOnBoot;
+    };
+
+    # Enable Blueman Bluetooth manager service
+    services.blueman.enable = true;
+
+    # Install Bluetooth management utilities
+    environment.systemPackages = with pkgsStable; [ blueman ];
+  };
+}
